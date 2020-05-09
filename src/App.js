c5976c3f0b9f4445b8058fbd1a19c5691e2a6815
VandersonAssis <assis.vanderson@gmail.com>
2020-05-09 15:33:24 -0300

    - Finished coding the sellers autocomplete component on the desktop

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ProductManagement from './components/products/ProductManagement';
 import Purchase from './components/purchase/Purchase';
+import Desktop from './components/ui/Desktop';
 
-function App() {
+export default function App() {
     return (
         <Router>
-            <Switch>
-                <Route path="/" exact component={ ProductManagement } />
-                <Route path="/purchase" exact component={ Purchase } />
+            <Switch >
+                <Desktop >
+                    <Route component={({ match }) =>
+                        <div>
+                            <Route path="/product" component={ProductManagement} />
+                            <Route path="/purchase" component={Purchase} />
+                        </div>
+                    } />
+                </Desktop>
             </Switch>
         </Router>
     );
 }
-
-export default App;
--- src/components/ui/Desktop.js
+++ src/components/ui/Desktop.js
@@ -2,27 +2,26 @@ import React from 'react';
 import './css/ApiAutocomplete.css';
 import './css/LoadingIndicator.css';
 import './css/Desktop.css';
-import { Grid, Paper } from '@material-ui/core';
+import { Grid, Box } from '@material-ui/core';
 
 export default function Desktop(props) {
     return (
-        <Grid container >
-            <Grid container className="top-menu-container" >
+        <Grid justify="center" container >
+            <Grid className="top-menu-container" container >
                 <Grid item sm={2} >
-                    <span className="top-menu-app-name" >Market Place</span>            
+                    <span className="top-menu-app-name" >Market Place</span>
                     <span className="top-menu-item" >Products</span>
                     <span className="top-menu-item" >Buy</span>
                 </Grid>
             </Grid>
-            <Grid container className="top-menu-bottom-container" ></Grid>
 
-            <Grid container >
-                asdf
+            <Grid className="top-menu-bottom-container" container />
+
+            <Grid className="content" container item sm={9} >
+                <Box marginTop={1} width="100%" >
+                    {props.children}
+                </Box>
             </Grid>
         </Grid>
-
-        // <div>
-        //     {props.children}
-        // </div>
     )
 }
