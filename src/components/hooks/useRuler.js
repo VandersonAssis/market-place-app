35283a5234441978ff204de3183411502d814b3c
VandersonAssis <assis.vanderson@gmail.com>
2020-05-16 21:53:06 -0300

    - Create a cool input validation and isolated it in the useRuler React hook

---
+++ src/components/hooks/useRuler.js
@@ -0,0 +1,74 @@
+const useRuler = () => {
+  const initializeRuler = (setRulesState, ruler) => {
+    for (var rulerEntry of ruler) {
+
+      setRulesState(
+        {
+          fieldName: rulerEntry.fieldName,
+          executeRule(value) {
+            let inconsistencies = new Array();
+            let rules;
+
+            for (var rulerT of ruler) {
+              if (rulerT.fieldName === this.fieldName) {
+                rules = rulerT.rules;
+                break;
+              }
+            }
+
+            for (var rule of rules) {
+              switch (rule.name) {
+                case 'range': {
+                  // Validating contraints set
+                  if (rule.constraints[0] === undefined || rule.constraints[0] === null ||
+                    rule.constraints[1] === undefined || rule.constraints[1] === null ||
+                    !!rule.constraints[0].toString().match('^(?=.*[A-Za-z])') || !!rule.constraints[1].toString().match('^(?=.*[A-Za-z])')) {
+
+                    throw new Error(`Contraints for ${rule.name} not correctly set!`);
+                  }
+
+                  if (value.length < rule.constraints[0] || value.length > rule.constraints[1])
+                    inconsistencies.push(`Length should range from ${rule.constraints[0]} to ${rule.constraints[1]}`);
+
+                  break;
+                } case 'contains': {
+                  // Validating contraints set
+                  if (rule.constraints[0] === undefined || rule.constraints[0] === null ||
+                    rule.constraints.length > 1 ||
+                    rule.constraints[0] !== 'letter') {
+
+                    throw new Error(`Contraints for ${rule.name} not correctly set!`);
+                  }
+
+                  if (rule.constraints[0] === 'letter' && !!!value.match('^(?=.*[A-Za-z])'))
+                    inconsistencies.push('Should contain at least one letter');
+
+                  break;
+                } default: {
+                  throw new Error(`Rule ${rule} not supported`);
+                }
+              }
+            }
+
+            return inconsistencies;
+          }
+        }
+      )
+    }
+  }
+
+  const validate = (rules, fieldName, value) => {
+    for (var rule of rules) {
+        if (rule.fieldName === fieldName) {
+            return rule.executeRule(value);
+        }
+    }
+};
+
+  return {
+    initializeRuler,
+    validate
+  };
+};
+
+export default useRuler;
--- src/components/products/AddNewProduct.js
+++ src/components/products/AddNewProduct.js
@@ -1,8 +1,9 @@
-import { ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary, FormControl, TextField, Grid, Box, Button } from '@material-ui/core';
-import { ExpandMore } from '@material-ui/icons';
-import React, { useState } from 'react';
+import { ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary, FormControl, TextField, Grid, Box, Button, unstable_StrictModeFade } from '@material-ui/core';
+import { ExpandMore, SmsOutlined } from '@material-ui/icons';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import useHttp from '../hooks/useHttp';
+import useRuler from '../hooks/useRuler';
 
 export default function AddNewProduct() {
     const { sendRequest } = useHttp();
@@ -13,15 +14,66 @@ export default function AddNewProduct() {
     const [enteredDescription, setEnteredDescription] = useState('');
     const [enteredPrice, setEnteredPrice] = useState();
     const [enteredQuantity, setEnteredQuantity] = useState();
+    const [rules, setRules] = useState([]);
+
+    const addNewRule = (newRule) => setRules(rules => [...rules, newRule]);
+
+    const { initializeRuler, validate } = useRuler();
+
+    useEffect(() => {
+        initializeRuler(addNewRule,
+            [
+                {
+                    fieldName: 'enteredName',
+                    rules: [
+                        { name: 'range', constraints: [3, 51] },
+                        { name: 'contains', constraints: ['letter'] }
+                    ]
+                },
+                {
+                    fieldName: 'enteredModel',
+                    rules: [
+                        { name: 'range', constraints: [1, 25] },
+                        { name: 'contains', constraints: ['letter'] }
+                    ]
+                },
+                {
+                    fieldName: 'enteredDescription',
+                    rules: [
+                        { name: 'range', constraints: [15, 255] },
+                        { name: 'contains', constraints: ['letter'] }
+                    ]
+                }                
+            ]
+        )
+    }, []);
 
     const submitNewProduct = event => {
         event.preventDefault();
-        
-        const newProduct = { idSeller: selectedSeller.id, model: enteredModel, name: enteredName, description: enteredDescription, 
-            price: enteredPrice, quantity: enteredQuantity };
 
-        sendRequest(process.env.REACT_APP_PRODUCTS_API_URL, 'POST', JSON.stringify(newProduct))
+        const newProduct = {
+            idSeller: selectedSeller.id, model: enteredModel, name: enteredName, description: enteredDescription,
+            price: enteredPrice, quantity: enteredQuantity
+        };
+
+        sendRequest(process.env.REACT_APP_PRODUCTS_API_URL, 'POST', JSON.stringify(newProduct));
+        clearFields();
     };
+
+    const clearFields = () => {
+        setEnteredName('');
+        setEnteredModel('');
+        setEnteredDescription('');
+        setEnteredPrice(0);
+        setEnteredQuantity(0);
+    };
+
+    const handleChange = (fieldName, setField, value) => {
+        setField(value);
+        let test = validate(rules, fieldName, value);
+        console.log(`Validate in newProduct ${test}`);
+        
+    }
 
     return (
         <ExpansionPanel disabled={selectedSeller === null || selectedSeller === ''} >
@@ -33,11 +85,11 @@ export default function AddNewProduct() {
                 <Box width="100%" >
                     <form onSubmit={submitNewProduct} >
                         <TextField className="text-field" label="Name" autoFocus={true} value={enteredName}
-                            onChange={event => { setEnteredName(event.target.value) }} />
+                            onChange={event => { handleChange('enteredName', setEnteredName, event.target.value) }} />
                         <br />
                         <TextField className="text-field" label="Model" value={enteredModel}
-                            onChange={event => { setEnteredModel(event.target.value) }} />
-                        <br />                        
+                            onChange={event => { handleChange('enteredModel', setEnteredModel, event.target.value) }} />
+                        <br />
                         <TextField className="text-field" label="Price" type="number" value={enteredPrice}
                             onChange={event => { setEnteredPrice(event.target.value) }} />
                         <br />
@@ -45,8 +97,8 @@ export default function AddNewProduct() {
                             onChange={event => { setEnteredQuantity(event.target.value) }} />
                         <br />
                         <TextField className="text-field" label="Description" multiline={true} rows="4" value={enteredDescription}
-                            onChange={event => { setEnteredDescription(event.target.value) }} />
-                        
+                            onChange={event => { handleChange('enteredDescription', setEnteredDescription, event.target.value) }} />
+
                         <br /> <br />
                         <Button color="primary" type="submit" variant="contained" >
                             REGISTER
