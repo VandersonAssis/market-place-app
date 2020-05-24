86fc2c428b14097da7ee57fd6622550aadc08943
VandersonAssis <assis.vanderson@gmail.com>
2020-05-24 16:31:11 -0300

    - Created the description accordion

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,7 @@ module.exports = function (grunt) {
                     "src/components/ui/css/ApiAutocomplete.css": "src/components/ui/css/ApiAutocomplete.less",
                     "src/components/products/css/ProductManagement.css": "src/components/products/css/ProductManagement.less",
                     "src/components/products/css/ProductList.css": "src/components/products/css/ProductList.less",
+                    "src/components/products/css/ProductListItem.css": "src/components/products/css/ProductListItem.less",
                     "src/components/products/css/AddNewProduct.css": "src/components/products/css/AddNewProduct.less"
                 }
             }
